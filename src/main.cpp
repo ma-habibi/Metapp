1721b6928ea1de81c91b72695b182375bb0ced62
Mahdi Habibi <em.habibi@hotmail.com>
2026-01-22 20:30:54 +0400

    #3 - New function parse_arguments

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <argparse/argparse.hpp>
+
+#include <filesystem>
+#include <iostream>
+
+void parse_arguments(argparse::ArgumentParser &program, int argc, char *argv[]) {
+  std::string cwd = std::filesystem::current_path();
+  program.add_argument("-t", "--todo")
+  .default_value(std::string(cwd.append("/resources/hello_world.yaml")));
+  program.add_argument("-d", "--database")
+  .default_value(std::string(cwd.append("/resources/database.db")));
+  try {
+    program.parse_args(argc, argv);
+  }
+  catch (const std::exception& err) {
+    std::cerr << err.what() << std::endl;
+    std::exit(1);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  argparse::ArgumentParser program(argv[0]);
+  parse_arguments(program, argc, argv);
+  return 0;
+}
