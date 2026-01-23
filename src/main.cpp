54e3447b45013fdb2c905883661a46a2cd0eba23
Mahdi Habibi <em.habibi@hotmail.com>
2026-01-23 09:11:17 +0400

    #3 - Add clang-fromat and documentations

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,34 @@
 #include <filesystem>
 #include <iostream>
 
-void parse_arguments(argparse::ArgumentParser &program, int argc, char *argv[]) {
+/**
+ * Parse the CLI arguments.
+ * @param program The reference to program object, that builds CLI arguments
+ * within itself.
+ * @param argc The argument count of the entry point.
+ * @param argv The vector of arguments from the entry point.
+ */
+void parse_arguments(argparse::ArgumentParser &program, int argc,
+                     char *argv[]) {
   std::string cwd = std::filesystem::current_path();
   program.add_argument("-t", "--todo")
-  .default_value(std::string(cwd.append("/resources/hello_world.yaml")));
+      .default_value(std::string(cwd.append("/resources/todo.yaml")));
   program.add_argument("-d", "--database")
-  .default_value(std::string(cwd.append("/resources/database.db")));
+      .default_value(std::string(cwd.append("/resources/example.db3")));
   try {
     program.parse_args(argc, argv);
-  }
-  catch (const std::exception& err) {
+  } catch (const std::exception &err) {
     std::cerr << err.what() << std::endl;
     std::exit(1);
   }
 }
 
 int main(int argc, char *argv[]) {
+
   argparse::ArgumentParser program(argv[0]);
   parse_arguments(program, argc, argv);
   std::filesystem::path todo_path{program.get<std::string>("todo")};
 
   YAML::Node todo_node = parse_todo::ParseTodo::read(todo_path);
-
   return 0;
 }
