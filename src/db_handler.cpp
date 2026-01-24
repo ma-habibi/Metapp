8e5225504eef7efaffdb4c9bf70e44eb2541301e
Mahdi Habibi <em.habibi@hotmail.com>
2026-01-24 17:14:45 +0400

    #3 - DbHandler - Read the YAML columns into a list

--- src/db_handler.cpp
+++ src/db_handler.cpp
@@ -25,6 +25,22 @@ SQLite::Database DbHandler::load_db(const std::filesystem::path &db_path) {
 
 SQLite::Database DbHandler::update_db(const std::filesystem::path &db_path, const YAML::Node &todo_node) {
   SQLite::Database loaded_db = load_db(db_path);
+
+  std::list<std::string> new_columns_from_yaml;
+  for (auto it = todo_node.begin(); it != todo_node.end(); ++it) {
+    try {
+      new_columns_from_yaml.push_back(it->first.as<std::string>());
+    }
+    catch (std::exception &e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+
+  std::cout << "New columns from YAML are: ";
+  std::cout << '[';
+  for (auto &col : new_columns_from_yaml)
+    std::cout << col << ", ";
+  std::cout << ']' << std::endl;
   return loaded_db;
 }
 } // namespace db_handler
