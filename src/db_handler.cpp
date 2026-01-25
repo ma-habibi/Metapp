5a0684f5ad5547e4b44255aee8f09a5ea2023477
Mahdi Habibi <em.habibi@hotmail.com>
2026-01-25 19:25:55 +0400

    #3 - DbHandler - Enhance the addition of tables

--- src/db_handler.cpp
+++ src/db_handler.cpp
@@ -38,49 +38,18 @@ SQLite::Database DbHandler::load_db(const std::filesystem::path &db_path) {
 
 SQLite::Database DbHandler::update_db(const std::filesystem::path &db_path,
                                       const YAML::Node &todo_node) {
-  std::list<std::string> tables_from_yaml;
-  for (auto it = todo_node.begin(); it != todo_node.end(); ++it) {
-    try {
-      tables_from_yaml.push_back(it->first.as<std::string>());
-    } catch (std::exception &e) {
-      std::cerr << e.what() << std::endl;
-    }
-  }
-
-  std::cout << "All tables read from YAML are: " << tables_from_yaml << "."
-            << std::endl;
-
   SQLite::Database loaded_db = load_db(db_path);
 
-  // Filter out to obtain non-existing tables
-  std::list<std::string> new_tables_from_yaml(tables_from_yaml);
-  SQLite::Statement select_tables_query(
-      loaded_db,
-      "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;");
-  while (select_tables_query.executeStep()) {
-    auto search = select_tables_query.getColumn(0).getString();
-    auto found = std::find(new_tables_from_yaml.begin(),
-                           new_tables_from_yaml.end(), search);
-    if (found != new_tables_from_yaml.end()) {
-      std::cout << "Table " << *found << " already exists in the table."
-                << std::endl;
-      new_tables_from_yaml.erase(found);
-    }
-  }
-  std::cout << "the tables from yaml which are new and must be inserted: "
-            << new_tables_from_yaml << "." << std::endl;
-
-  for (auto &table : new_tables_from_yaml) {
-    try {
-      std::cout << "Creating table " << table << "." << std::endl;
-      ;
-      std::stringstream cmd_stream;
-      cmd_stream << "CREATE TABLE IF NOT EXISTS " << table
-                 << "(id INTEGER PRIMARY KEY)";
-      std::string cmd = cmd_stream.str();
+  for (auto it = todo_node.begin(); it != todo_node.end(); ++it) {
+    auto yaml_section = it->first.as<std::string>();
+    if (loaded_db.tableExists(yaml_section)) {
+      std::cout << "Table " << yaml_section
+                << " already exists in the database." << std::endl;
+    } else {
+      std::cout << "Creating new table " << yaml_section << "." << std::endl;
+      std::string cmd =
+          "CREATE TABLE " + yaml_section + "(id INTEGER PRIMARY KEY)";
       loaded_db.exec(cmd);
-    } catch (std::exception &e) {
-      std::cerr << e.what() << std::endl;
     }
   }
   return loaded_db;
